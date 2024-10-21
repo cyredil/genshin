33b46ce78c6338038b8d85f2a5befb75971044db
crdl_arml <lyderic.gandibleux@gmail.com>
2024-10-21 20:30:20 +0200

    better arrengment with an improvment of authentication.py

--- dailies/credentials.py
+++ dailies/credentials.py
@@ -2,51 +2,8 @@ import genshin
 import tkinter as tk
 import configparser
 
-def load_credentials(filename):
-    config = configparser.ConfigParser()
-    config.read(filename)
-    username = config.get('Credentials', 'username', fallback='')
-    password = config.get('Credentials', 'password', fallback='')
-    save_username = config.get('Settings', 'save_username',
-                               fallback='')
-    save_password = config.get('Settings', 'save_password',
-                               fallback='')
-    browser = config.get('Settings', 'browser', fallback='')
+from dailies.bin.bin import load_credentials, save_credentials, set_middle
 
-    return username, password, save_username, save_password, browser
-
-def save_credentials(filename, credentials):
-    config = configparser.ConfigParser()
-    config.read(filename)
-
-    config['Credentials'] = {
-        'username': credentials[0],
-        'password': credentials[1]
-    }
-    config['Settings'] = {
-        'save_username': credentials[2],
-        'save_password': credentials[3],
-        'browser': credentials[4]
-    }
-
-    with open('dailies/credentials.ini', 'w') as configfile:
-        config.write(configfile)
-
-def set_middle(master):
-    # Set the desired size for the window
-    width = 300   # Width of the window
-    height = 200  # Height of the window
-
-    # Get the screen width and height
-    screen_width = master.winfo_screenwidth()
-    screen_height = master.winfo_screenheight()
-
-    # Calculate the x and y coordinates to center the window
-    x = (screen_width // 2) - (width // 2)
-    y = (screen_height // 2) - (height // 2)
-
-    # Set the geometry of the window
-    master.geometry(f"{width}x{height}+{x}+{y}")
 
 def credentials_gui():
 
@@ -67,7 +24,7 @@ def credentials_gui():
     mail.insert(0, cred[0])
     mail.grid(row=1, column=1)
 
-    password = tk.Entry(master)
+    password = tk.Entry(master, show='*')
     password.insert(0, cred[1])
     password.grid(row=2, column=1)
 
