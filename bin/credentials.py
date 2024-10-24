f970675bba0c5009cf8efcb7d5cbac014e2a9cd0
crdl_arml <lyderic.gandibleux@gmail.com>
2024-10-24 02:27:12 +0200

    Commenting project, and small english corrections (NOT FULLY CORRECTED)

--- bin/credentials.py
+++ bin/credentials.py
@@ -1,42 +1,55 @@
+
+# importing packages
 import tkinter as tk
 from .utils import load_credentials, save_credentials, set_middle
 
 def credentials_gui():
+    """GUI made with tkinter to ask user usefull credentials"""
 
+    # Loading already present credentials ('' for each empty fields)
     cred = list(load_credentials('bin/credentials.ini'))
 
     # Creating widget
     master = tk.Tk()
 
+    # Setting widget as the focused and top layer windows
     master.attributes("-topmost", True)
     master.focus_force()
 
+    # Setting title
     master.title('Credentials.exe')
 
-    explication = tk.Message(master, text="BONJOUR", width=250)
+    # First row : Asking user for credentials
+    text_please = "Please enter your credentials for the selected game :"
+    explication = tk.Message(master,
+                             text=text_please,
+                             width=250)
     explication.grid(row=0, columnspan=2)
 
+    # Creating the entry for mail and password
     tk.Label(master, text="email :").grid(row=1)
     tk.Label(master, text="password").grid(row=2)
-
     mail = tk.Entry(master)
     mail.insert(0, cred[0])
     mail.grid(row=1, column=1)
 
-    password = tk.Entry(master, show='*')
+    password = tk.Entry(master, show='*') # Hide password entry
     password.insert(0, cred[1])
     password.grid(row=2, column=1)
 
+    # Saving the user chose for the 'saving mail' choice
     mail_save_var = tk.IntVar(value=cred[2])
     mail_save = tk.Checkbutton(master, text='Save mail',
                                variable=mail_save_var)
     mail_save.grid(row=3, sticky='W')
 
+    # Saving the user chose for the 'saving password' choice
     password_save_var = tk.IntVar(value=cred[3])
     password_save = tk.Checkbutton(master, text='Save password',
                                    variable=password_save_var)
     password_save.grid(row=4, sticky='W')
 
+    # Creating the browser radio button
     browser = tk.IntVar(value=cred[4])
     brow1 = tk.Radiobutton(master, text='Chrome',
                            variable=browser, value=0)
@@ -45,6 +58,7 @@ def credentials_gui():
                            variable=browser, value=1)
     brow2.grid(row=5, column=1)
 
+    # Creating the game radio button
     game = tk.IntVar(value=cred[5])
     game1 = tk.Radiobutton(master, text='Genshin Impact',
                            variable=game, value=0)
@@ -56,6 +70,7 @@ def credentials_gui():
                            variable=game, value=2)
     game2.grid(row=6, column=2)
 
+    # Creating the region radio button
     region = tk.IntVar(value=cred[6])
     regi1 = tk.Radiobutton(master, text='Not China',
                            variable=region, value=0)
@@ -64,7 +79,11 @@ def credentials_gui():
                            variable=region, value=1)
     regi2.grid(row=7, column=1)
 
+    # Defining the 'on_send' reaction to clicking a 'send' button
     def on_send():
+        """Saving Credentials before destroying the widget"""
+
+        # Saving creds into a list compatible with save_credentials
         cred = [mail.get(),
                 password.get(),
                 mail_save_var.get(),
@@ -72,19 +91,26 @@ def credentials_gui():
                 browser.get(),
                 game.get(),
                 region.get()]
+        
+        # Saving 'save_credentials'
         save_credentials('bin/credentials.ini', cred)
+
+        # Destroying widget
         master.destroy()
 
+    # Creating the save and exit button
     save_and_exit_button = tk.Button(master, text='Send', width=25,
                                      command=on_send)
     save_and_exit_button.grid(row=8, columnspan=2)
-    save_and_exit_button.focus_force()
+    save_and_exit_button.focus_force() # Set as default activ button
 
+    # Binding 'Enter' key to invoke button
     master.bind('<Return>', lambda event: save_and_exit_button.invoke())
 
-
+    # Setting master in the middle of the screen
     set_middle(master)
 
+    # Running master
     master.mainloop()
 
 
--- bin/promo_codes.py
+++ bin/promo_codes.py
@@ -1,3 +1,5 @@
+
+# importing packages
 import requests
 import genshin
 import time
@@ -7,11 +9,23 @@ import asyncio
 
 
 def scrap_promo_codes():
+    """Function to scrap all promo codes available in
+    'https://www.pockettactics.com/genshin-impact/codes'"""
+
+    # Setting page requested
     page = requests.get('https://www.pockettactics.com/genshin-impact/codes')
+
+    # Parsing page
     soup = bs(page.content, 'html.parser')
 
+    # Selecting all lists
     lines = soup.findAll('li')
 
+    # Separating lines in two categories:
+    #   strong ones: usually recent code
+    #   classic ones: usually old code
+    # And only keeping the line with a '\u2013' separator (used to separate
+    # code and rewards)
     strong_lines = [li for li in lines if (
         li.find('strong') and '–' in li.text)]
     classic_lines = [li for li in lines if (
@@ -19,6 +33,8 @@ def scrap_promo_codes():
 
     codes = []
 
+    # Adding every strong lines to code with the game type, the code,
+    # the awards and its recentness
     for line in strong_lines:
         game = 'genshin'
         code = line.find('strong').text
@@ -26,6 +42,8 @@ def scrap_promo_codes():
         recent = 'new'
         codes.append([game, code, award, recent])
 
+    # Adding every recent lines to code with the game type, the code,
+    # the awards and its recentness
     for line in classic_lines:
         game = 'genshin'
         line = line.text.split('–')
@@ -34,19 +52,37 @@ def scrap_promo_codes():
         recent = 'old'
         codes.append([game, code, award, recent])
 
+    # Saving codes in a promo_codes.csv
     data = pd.DataFrame(codes, columns=['game', 'code', 'award', 'recent'])
     data.to_csv('bin/promo_codes.csv')
 
 async def test_promo_code(code, award, client):
+    """function to test a given promo code
+
+    Args:
+        code (str): Code to try out
+        award (str): Expected award string to display
+        client (genshin.client): Genshin client already logged in
+    
+    Returns:
+        str: String with the test code result with detailed.
+            (Code cain be 'claimable', 'Already claimed', 'Expired' or 
+            Invalid)
+    """
+
+    # Waiting 5 seconds to test code to avoid genshin blocking the claim
+    # (Due to too much claims)
     print(f'BOT: Testing code *{code}*...')
     time.sleep(5)
+
+    # Try to redeem code with corresponding output
     try :
         await client.redeem_code(code)
-        return (f'BOT: Code *{code}* claimed! You earned {award}!')
+        return (f'BOT: Succes! Code *{code}* claimed! You earned {award}!')
     except genshin.RedemptionClaimed:
         return f'BOT: *{code}* already claimed! You thief!'
     except genshin.RedemptionInvalid as e:
-        if '-2001' in str(e):
+        if '-2001' in str(e): # -2001 correspond to an expired code
             return 'BOT: Code as Expired...'
         else:
             return 'BOT: Code is invalid...?'
@@ -55,13 +91,34 @@ async def test_promo_code(code, award, client):
 
 
 def test_promo_codes(df, client, new_only=True):
+    """Testing all promo code on a client given the code dataframe 
+    corresponding.
+
+    Only test condes listed as activ by default (to avoid long run time)
+
+    Args:
+        df (pandas.DataFrame): Dataframe with all codes.
+            (Should be generated using `scrap_promo_codes`)
+        client (genshin.client): Genshin client succesfully logged in
+        new_only (bool, optional): True (default) if function should test
+            new codes only or old and new codes.
+    """
+
+    # Reading df
     df = pd.read_csv('bin/promo_codes.csv')
+
+    # Kepping only new codes if new_only is True
     if new_only:
         df = df.loc[df['recent'] == 'new']
-        
+    
+    count = 0
+    # Claiming every code in df
     for ind, row in df.iterrows():
-        print(asyncio.run(test_promo_code(row['code'], row['award'], client)))
-    print('BOT: No more new codes to try out!')
+        r = asyncio.run(test_promo_code(row['code'], row['award'], client))
+
+        count += int('succes' in r) # counting the number of succes
+        print(r)
+    print(f'BOT: No more new codes to try out! You redeemed {count} codes!')
 
 if __name__ == '__main__':
     df = scrap_promo_codes()
